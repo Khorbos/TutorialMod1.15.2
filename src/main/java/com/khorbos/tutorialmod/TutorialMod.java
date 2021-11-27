de0eddaf235f368aca9c60b1e4d947b5a8125e78
Khorbos <korbos.pro@gmail.com>
2021-11-27 20:40:13 +0100

    Initial commit

---
+++ src/main/java/com/khorbos/tutorialmod/TutorialMod.java
@@ -0,0 +1,33 @@
+package com.khorbos.tutorialmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(TutorialMod.ID)
+public class TutorialMod
+{
+    public static final String ID = "tutorialmod";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public TutorialMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {}
+
+    private void doClientStuff(final FMLClientSetupEvent event) {}
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {}
+}
