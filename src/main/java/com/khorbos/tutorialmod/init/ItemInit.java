f90e9f3c62d90c45162bba86a6100a2fc667073d
Khorbos <korbos.pro@gmail.com>
2021-11-28 14:44:47 +0100

    feat(tools): add an example material and add a set of tools with this new material

--- src/main/java/com/khorbos/tutorialmod/init/ItemInit.java
+++ src/main/java/com/khorbos/tutorialmod/init/ItemInit.java
@@ -2,8 +2,8 @@ package com.khorbos.tutorialmod.init;
 
 import com.khorbos.tutorialmod.TutorialMod;
 import com.khorbos.tutorialmod.TutorialMod.TutorialItemGroup;
-import net.minecraft.item.Food;
-import net.minecraft.item.Item;
+import com.khorbos.tutorialmod.util.enums.ModItemTier;
+import net.minecraft.item.*;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraftforge.fml.RegistryObject;
@@ -17,4 +17,10 @@ public class ItemInit {
     public static final RegistryObject<Item> EDIBLE_ITEM = ITEMS.register("edible_item", () -> new Item(new Item.Properties().food(new Food.Builder().hunger(8).saturation(1.2F).build()).group(TutorialItemGroup.instance)));
     public static final RegistryObject<Item> EDIBLE_MEAT_ITEM = ITEMS.register("edible_meat_item", () -> new Item(new Item.Properties().food(new Food.Builder().hunger(1).saturation(0.2F).meat().build()).group(TutorialItemGroup.instance)));
     public static final RegistryObject<Item> EDIBLE_EFFECT_ITEM = ITEMS.register("edible_effect_item", () -> new Item(new Item.Properties().food(new Food.Builder().hunger(1).saturation(0.2F).effect(new EffectInstance(Effects.SPEED, 6000, 1), 1).build()).group(TutorialItemGroup.instance)));
+
+    public static final RegistryObject<Item> EXAMPLE_SWORD = ITEMS.register("example_sword", () -> new SwordItem(ModItemTier.EXAMPLE, 7, 5.0F, new Item.Properties().group(TutorialItemGroup.instance)));
+    public static final RegistryObject<Item> EXAMPLE_PICKAXE = ITEMS.register("example_pickaxe", () -> new PickaxeItem(ModItemTier.EXAMPLE, 4, 5.0F, new Item.Properties().group(TutorialItemGroup.instance)));
+    public static final RegistryObject<Item> EXAMPLE_AXE = ITEMS.register("example_axe", () -> new AxeItem(ModItemTier.EXAMPLE, 11, 3.0F, new Item.Properties().group(TutorialItemGroup.instance)));
+    public static final RegistryObject<Item> EXAMPLE_SHOVEL = ITEMS.register("example_shovel", () -> new ShovelItem(ModItemTier.EXAMPLE, 2, 5.0F, new Item.Properties().group(TutorialItemGroup.instance)));
+    public static final RegistryObject<Item> EXAMPLE_HOE = ITEMS.register("example_hoe", () -> new HoeItem(ModItemTier.EXAMPLE, 5.0F, new Item.Properties().group(TutorialItemGroup.instance)));
 }
---
+++ src/main/java/com/khorbos/tutorialmod/util/enums/ModItemTier.java
@@ -0,0 +1,60 @@
+package com.khorbos.tutorialmod.util.enums;
+
+import com.khorbos.tutorialmod.init.ItemInit;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+    EXAMPLE(4, 1500, 15.0F, 7.0F, 250, () -> {
+        return Ingredient.fromItems(ItemInit.EXAMPLE_ITEM.get());
+    });
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    private ModItemTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
+        this.harvestLevel = harvestLevelIn;
+        this.maxUses = maxUsesIn;
+        this.efficiency = efficiencyIn;
+        this.attackDamage = attackDamageIn;
+        this.enchantability = enchantabilityIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.getValue();
+    }
+}
