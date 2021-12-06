ee5ea4e0b2db74f0758e8f9b5cd3139e1e0f3cbb
Khorbos <korbos.pro@gmail.com>
2021-12-06 01:41:55 +0100

    feat(NBT): add NBTHelper class for write/read methods from tile entities class

---
+++ src/main/java/com/khorbos/tutorialmod/util/helper/NBTHelper.java
@@ -0,0 +1,44 @@
+package com.khorbos.tutorialmod.util.helper;
+
+import com.khorbos.tutorialmod.tileentity.QuarryTileEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class NBTHelper {
+    public static CompoundNBT toNBT(Object o){
+        if (o instanceof ItemStack){
+            return writeItemStack((ItemStack) o);
+        }
+        return null;
+    }
+
+    @Nullable
+    public static Object fromNBT(@Nonnull CompoundNBT compound) {
+        switch (compound.getByte("type")) {
+            case 0:
+                return readItemStack(compound);
+            default:
+                return null;
+        }
+    }
+
+    private static CompoundNBT writeItemStack(ItemStack itemStack) {
+        CompoundNBT compoundNBT = new CompoundNBT();
+        compoundNBT.putInt("count", itemStack.getCount());
+        compoundNBT.putString("item", itemStack.getItem().getRegistryName().toString());
+        compoundNBT.putByte("type", (byte)0);
+        return compoundNBT;
+    }
+
+    private static ItemStack readItemStack(CompoundNBT compound) {
+        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(compound.getString("item")));
+        int count = compound.getInt("count");
+        return new ItemStack(item, count);
+    }
+}
