e625bebf4606b59cad942256c82b750c1b7e1734
Khorbos <korbos.pro@gmail.com>
2021-12-07 23:23:02 +0100

    feat(slab block): add an example of a slab block

--- src/main/java/com/khorbos/tutorialmod/init/BlockInit.java
+++ src/main/java/com/khorbos/tutorialmod/init/BlockInit.java
@@ -5,6 +5,7 @@ import com.khorbos.tutorialmod.objects.blocks.CustomBlock;
 import com.khorbos.tutorialmod.objects.blocks.QuarryBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.FenceBlock;
+import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
@@ -17,6 +18,7 @@ public class BlockInit {
 
     public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(Block.Properties.create(Material.WOOD)));
     public static final RegistryObject<Block> EXAMPLE_STAIRS = BLOCKS.register("example_stairs", () -> new StairsBlock(() -> BlockInit.EXAMPLE_BLOCK.get().getDefaultState(), Block.Properties.create(Material.WOOD)));
+     public static final RegistryObject<Block> EXAMPLE_SLAB = BLOCKS.register("example_slab", () -> new SlabBlock(Block.Properties.create(Material.IRON)));
     public static final RegistryObject<Block> EXAMPLE_FENCE = BLOCKS.register("example_fence", () -> new FenceBlock(Block.Properties.create(Material.IRON, MaterialColor.IRON)));
 
     public static final RegistryObject<CustomBlock> CUSTOM_BLOCK = BLOCKS.register("custom_block", () -> new CustomBlock());
